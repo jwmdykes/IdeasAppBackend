8acd5241d2312af11325ca81b53bea1942f3cd93
John Dykes <98johndykes@gmail.com>
2023-11-30 20:35:05 -0500

    playing around with entity framework core and postgresql

---
+++ IdeasContext.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdeasAppBackend;
+
+public class IdeasContext : DbContext
+{
+    public DbSet<Idea> Ideas { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(@"Host=localhost;Username=localadmin;Password=password;Database=ideasdb").UseSnakeCaseNamingConvention().UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
+}
+
+public class Idea
+{
+    public override string ToString() => IdeaText;
+
+    [Key]
+    public int IdeaId { get; set; }
+    public string IdeaText { get; set; } = "";
+
+    public uint Likes { get; set; }
+    public uint Dislikes { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime EditDate { get; set; }
+
+    public Idea()
+    {
+        CreationDate = DateTime.UtcNow;
+        EditDate = DateTime.UtcNow;
+    }
+}
+
+public class Comment
+{
+    [Key]
+    public int CommentId { get; set; }
+    public string CommentText { get; set; } = "";
+    public int IdeaId { get; set; }
+    public int AuthorId { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime EditDate { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using IdeasAppBackend;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+await using var ctx = new IdeasContext();
+// our user doesn't have permission to 
+// create and delete the database!
+await ctx.Database.EnsureDeletedAsync();
+await ctx.Database.EnsureCreatedAsync();
 
-app.Run();
+ctx.Ideas.Add(new Idea() {IdeaText = "My fantastic idea!"});
+
+await ctx.SaveChangesAsync();
+
+var res = await ctx.Ideas.Where(t => t.IdeaText.StartsWith("M")).ToListAsync();
+
+Console.WriteLine("Here are the ideas that start with M!\n\n");
+Console.WriteLine(string.Join("\n", res));
+
+Console.WriteLine("Let's like all of them!");
+
+foreach (Idea idea in res)
+{
+    idea.Likes = 123;
+    idea.Dislikes = 255;
+    idea.EditDate = DateTime.UtcNow;
+} 
+
+await ctx.SaveChangesAsync();
